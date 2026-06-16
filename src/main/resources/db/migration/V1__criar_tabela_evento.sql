3a2835b467a41fb4f9f3218006889a56edf4f48c
igorpandolf <pandolf.igor@gmail.com>
2026-06-16 19:13:41 -0300

    Add migration V1__criar_tabela_evento.sql

--- src/main/resources/db/migration/V1__criar_tabela_evento.sql
+++ src/main/resources/db/migration/V1__criar_tabela_evento.sql
@@ -2,11 +2,12 @@
 CREATE TABLE Eventos (
     id BIGSERIAL PRIMARY KEY ,
     nome VARCHAR(255) NOT NULL,
-    descricao VARCHAR(255),
-    inicio DATE NOT NULL,
-    fim DATE,
-    identificador VARCHAR(255) NOT NULL,
-    local VARCHAR(255) NOT NULL,
-    organizador VARCHAR(255),
+    descricao TEXT,
+    data_inicio TIMESTAMP NOT NULL,
+    data_fim TIMESTAMP NOT NULL,
+    identificador VARCHAR(255) NOT NULL UNIQUE ,
+    local_evento VARCHAR(255) NOT NULL,
+    organizador VARCHAR(255) NOT NULL,
+    capacidade INTEGER NOT NULL,
     tipoEvento VARCHAR(50) NOT NULL
 )
